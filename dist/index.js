e19c0a4af2c6dc85b988f1011c8ca2c999d57ed4
Nazaire <dylan.kay@me.com>
2023-07-25 11:12:09 +0800

    Ignore dist git

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-export { Consumer } from "./types/Consumer.js";
-export { Composer } from "./types/Composer.js";
-export * as Network from "./types/Network/index.js";
-export { Instrument } from "./types/Instrument.js";
-export { Worker } from "./types/Worker/Worker.js";
-export { Workspace } from "./types/Workspace.js";
-//# sourceMappingURL=index.js.map
